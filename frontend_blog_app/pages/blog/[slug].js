0e06de4a119e8ae16f539469e0abc28218469444
Nur Ahmed <06nurahmed@gmail.com>
2024-09-01 13:17:06 +0600

    Update [slug].js

--- frontend_blog_app/pages/blog/[slug].js
+++ frontend_blog_app/pages/blog/[slug].js
@@ -4,17 +4,30 @@ import { AiOutlineDeploymentUnit, AiOutlineUser } from "react-icons/ai";
 import { PiMedalFill } from "react-icons/pi";
 import { SlCalender } from "react-icons/sl";
 import Link from "next/link";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { useRouter } from "next/router";
-import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { a11yDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { a11yDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 import Head from "next/head";
-import Comments from "@/components/Comments";
+import { FiDatabase } from "react-icons/fi";
+import { TbBrandNextjs } from "react-icons/tb";
+import { FaGithub, FaHtml5, FaInstagram, FaTwitter } from "react-icons/fa6";
 import RightPortfolioInfo from "@/components/RightPortfolioInfo";
 import { RightTopicSection } from "@/components/RightTopicSection";
+import Comments from "@/components/Comments";
+import axios from 'axios';
+
+export async function getServerSideProps(context) {
+    const { slug } = context.params;
+    let blog = null;
+    try {
+        const res = await axios.get(`${process.env.SITE_URL}/api/getblog?slug=${slug}`);
+        blog = res.data[0] || null;
+    } catch (error) {
+        console.error("Error fetching blog:", error);
+    }
+    return { props: { blog } };
+}
 
 function extractFirstImageUrl(markdownContent) {
     if (!markdownContent || typeof markdownContent !== "string") {
@@ -26,28 +39,13 @@ function extractFirstImageUrl(markdownContent) {
     return match ? match[1] : null;
 }
 
-export default function BlogPage() {
-    const router = useRouter();
-    const { slug } = router.query;
-    const [blog, setBlog] = useState(null);
-    const [loading, setLoading] = useState(true);
+export default function BlogPage({ blog }) {
 
-    useEffect(() => {
-        if (slug) {
-            axios.get(`/api/getblog?slug=${slug}`).then(res => {
-                const alldata = res.data;
-                setBlog(alldata);
-                setLoading(false);
-            }).catch(error => {
-                console.error("Error fetching blog:", error);
-                setLoading(false); // Stop loading even if there's an error
-            });
-        }
-    }, [slug]);
+    const router = useRouter();
+    const [copied, setCopied] = useState(false);
 
     const Code = ({ node, inline, className, children, ...props }) => {
         const match = /language-(\w+)/.exec(className || '');
-        const [copied, setCopied] = useState(false);
 
         const handleCopy = () => {
             navigator.clipboard.writeText(children);
@@ -85,89 +83,85 @@ export default function BlogPage() {
         }
     };
 
+    if (!blog) {
+        return <div>Blog not found.</div>;
+    }
+
+    const imageUrl = extractFirstImageUrl(blog.description) || '/img/noimage.jpg';
+
     return (
         <>
-            {blog && (
-                <Head>
-                    <title>{blog.title ? `${blog.title} | Robo Superior` : 'Loading...'}</title>
-                    <meta name="description" content={blog.metadescription || 'Loading...'} />
-                    <meta name="keywords" content={blog.keywords || 'Technology, Robotics, AI, Blogs'} />
-                    <meta name="author" content={blog.author || 'RoboSuperior'} />
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <Head>
+                <title>{blog.title ? `${blog.title} | Robo Superior` : 'Loading...'}</title>
+                <meta name="description" content={blog.metadescription || 'Loading...'} />
+                <meta name="keywords" content={`${blog.keywords || 'Technology, Robotics, AI, Blogs'}`} />
+                <meta name="author" content={blog.author || 'RoboSuperior'} />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-                    <meta property="og:locale" content="en_US" />
-                    <meta property="og:type" content="article" />
-                    <meta property="og:title" content={blog.title ? `${blog.title} | Robo Superior` : 'Loading...'} />
-                    <meta property="og:description" content={blog.metadescription || 'Loading...'} />
-                    <meta property="og:url" content={process.env.SITE_URL} />
-                    <meta property="og:site_name" content="Robo Superior" />
-                    <meta property="og:image" content={extractFirstImageUrl(blog.description) || '/img/noimage.jpg'} />
-                    <meta property="article:published_time" content={blog.createdAt ? new Date(blog.createdAt).toISOString() : ''} />
+                <meta property="og:locale" content="en_US" />
+                <meta property="og:type" content="article" />
+                <meta property="og:title" content={blog.title ? `${blog.title} | Robo Superior` : 'Loading...'} />
+                <meta property="og:description" content={blog.metadescription || 'Loading...'} />
+                <meta property="og:url" content={process.env.SITE_URL} />
+                <meta property="og:site_name" content="Robo Superior" />
+                <meta property="og:image" content={imageUrl} />
+                <meta property="article:published_time" content={blog.createdAt ? new Date(blog.createdAt).toISOString() : ''} />
 
-                    <meta name="twitter:card" content="summary_large_image" />
-                    <meta name="twitter:site" content="@RoboSuperior" />
-                    <meta name="twitter:title" content={blog.title} />
-                    <meta name="twitter:description" content={blog.metadescription} />
-                    <meta name="twitter:image" content={extractFirstImageUrl(blog.description) || '/img/noimage.jpg'} />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:site" content="@RoboSuperior" />
+                <meta name="twitter:title" content={blog.title} />
+                <meta name="twitter:description" content={blog.metadescription} />
+                <meta name="twitter:image" content={imageUrl} />
 
-                    <link rel="canonical" href={`${process.env.SITE_URL}/${slug}`} />
+                <link rel="canonical" href={`${process.env.SITE_URL}/${router.query.slug}`} />
 
-                    <script type="application/ld+json">
-                        {`{
-                            "@context": "https://schema.org",
-                            "@type": "BlogPosting",
-                            "mainEntityOfPage": {
-                                "@type": "WebPage",
-                                "@id": "${process.env.SITE_URL}/${slug}"
-                            },
-                            "headline": "${blog.title}",
-                            "description": "${blog.metadescription}",
-                            "image": "${extractFirstImageUrl(blog.description) || '/default-image.png'}",
-                            "author": {
-                                "@type": "Organization",
-                                "name": "Robo Superior"
-                            },
-                            "publisher": {
-                                "@type": "Organization",
-                                "name": "Robo Superior",
-                                "logo": {
-                                    "@type": "ImageObject",
-                                    "url": "/logo.png"
-                                }
-                            },
-                            "datePublished": "${blog.createdAt ? new Date(blog.createdAt).toISOString() : ''}"
-                        }`}
-                    </script>
-                </Head>
-            )}
+                <script type="application/ld+json">
+                    {`{
+                        "@context": "https://schema.org",
+                        "@type": "BlogPosting",
+                        "mainEntityOfPage": {
+                            "@type": "WebPage",
+                            "@id": "${process.env.SITE_URL}/${router.query.slug}"
+                        },
+                        "headline": "${blog.title}",
+                        "description": "${blog.metadescription}",
+                        "image": "${imageUrl}",
+                        "author": {
+                            "@type": "Organization",
+                            "name": "Robo Superior"
+                        },
+                        "publisher": {
+                            "@type": "Organization",
+                            "name": "Robo Superior",
+                            "logo": {
+                                "@type": "ImageObject",
+                                "url": "/logo.png"
+                            }
+                        },
+                        "datePublished": "${blog.createdAt ? new Date(blog.createdAt).toISOString() : ''}"
+                    }`}
+                </script>
+            </Head>
 
             <div className="slugpage">
                 <div className="container">
                     <div className="topslug_titles" data-aos="fade-right">
-                        <h1 className="slugtitle">{loading ? 'Loading...' : blog?.title}</h1>
-                        <h5>By <span>RoboSuperior</span>・ Published in <span>{loading ? 'Loading...' : blog?.blogcategory}</span> ・ {blog && new Date(blog.createdAt).toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' })}
+                        <h1 className="slugtitle">{blog.title}</h1>
+                        <h5>By <span>RoboSuperior</span>・ Published in <span>{blog.blogcategory}</span> ・ {new Date(blog.createdAt).toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' })}
                             <span>・ 1 min read</span></h5>
                     </div>
 
                     <div className="flex flex-sb flex-left pb-5 flex-wrap">
                         <div className="leftblog_data_markdown" data-aos="fade-up">
-                            {loading ? (
-                                <div className='wh-100 flex flex-center mt-3'>
-                                    <div aria-live="assertive" role="alert" className="loader"></div>
-                                </div>
-                            ) : (
-                                <div className="w-100 blogcontent">
-                                    <ReactMarkdown
-                                        remarkPlugins={[remarkGfm]}
-                                        components={{
-                                            code: Code,
-                                        }}
-                                    >
-                                        {blog.description}
-                                    </ReactMarkdown>
-                                </div>
-                            )}
-                            <Comments slug={slug} />
+                            <div className="w-100 blogcontent">
+                                <ReactMarkdown
+                                    remarkPlugins={[remarkGfm]}
+                                    components={{ code: Code }}
+                                >
+                                    {blog.description}
+                                </ReactMarkdown>
+                            </div>
+                            <Comments slug={router.query.slug} />
                         </div>
                         <div className="rightslug_data">
                             <RightPortfolioInfo />
