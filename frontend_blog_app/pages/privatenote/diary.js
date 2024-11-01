9785e321e1c574aa753d2ea7699717f17e06d8fc
Nur Ahmed <06nurahmed@gmail.com>
2024-11-01 18:19:08 +0600

    diary done

--- frontend_blog_app/pages/privatenote/diary.js
+++ frontend_blog_app/pages/privatenote/diary.js
@@ -10,7 +10,7 @@ import React from "react";
 import { jwtDecode } from "jwt-decode";
 import Head from "next/head";
 import toast from "react-hot-toast";
-import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Input, Textarea } from "@nextui-org/react";
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure, Input, Textarea, Select, SelectItem } from "@nextui-org/react";
 import axios from "axios"; // Ensure axios is imported
 
 export default function userDiary() {
@@ -30,6 +30,8 @@ export default function userDiary() {
     const [transactionType, setTransactionType] = useState('');
     const [reason, setReason] = useState('');
     const [cost, setCost] = useState();
+    const [searchItem, setSearchItem] = useState();
+    const [transType, setTransType] = useState();
     const [waiting, setWaiting] = useState(false);
     const [editingNoteId, setEditingNoteId] = useState(null); // Track the note being edited
 
@@ -62,14 +64,7 @@ export default function userDiary() {
         checkUser();
     }, [router]);
 
-    // useEffect(() => {
-    //     setOption('');
-    //     setNote('');
-    //     setTransactionType('');
-    //     setReason('');
-    //     setCost('');
-    // }, [onClose, handleOpen])
-    function closeReset(){
+    function closeReset() {
         setOption('');
         setNote('');
         setTransactionType('');
@@ -96,13 +91,52 @@ export default function userDiary() {
         }
     }, [editingNoteId, allNotes]);
 
+    // Filter blogs by author first, then by search query
+    const filteredBlogs = useMemo(() => {
+        // Helper function to filter by search query
+        const filterBySearchQuery = (notes, key) => {
+            return notes.filter(note =>
+                note[key] && note[key].toLowerCase().includes(searchQuery.toLowerCase())
+            );
+        };
+
+        if (searchItem === "$.1") {
+            // Filtering by 'cost' with transaction type (debit/credit/all)
+            const costNotes = allNotes.filter(note => note.cost);
+
+            if (transType === "debit") {
+                return filterBySearchQuery(
+                    costNotes.filter(note => note.transactionType === "debit"),
+                    "reason"
+                );
+            } else if (transType === "credit") {
+                return filterBySearchQuery(
+                    costNotes.filter(note => note.transactionType === "credit"),
+                    "reason"
+                );
+            } else {
+                return filterBySearchQuery(costNotes, "reason");
+            }
+        } else if (searchItem === "$.2") {
+            // Filtering by 'note'
+            return filterBySearchQuery(allNotes.filter(note => note.note), "note");
+        } else {
+            // Default: Filtering all notes by 'note'
+            return allNotes.filter(note =>
+                note.note.toLowerCase().includes(searchQuery.toLowerCase())
+            );
+        }
+    }, [searchItem, searchQuery, allNotes, transType]);
+
+
+
     // Calculate total number of filtered blogs
-    const totalFilteredBlogs = allNotes.length;
+    const totalFilteredBlogs = filteredBlogs.length;
 
     // Calculate the currently displayed blogs with pagination logic
     const indexOfFirstBlog = (currentPage - 1) * perPage;
     const indexOfLastBlog = currentPage * perPage;
-    const currentBlogs = allNotes.slice(indexOfFirstBlog, indexOfLastBlog);
+    const currentBlogs = filteredBlogs.slice(indexOfFirstBlog, indexOfLastBlog);
 
     // Pagination page numbers calculation
     const pageNumbers = useMemo(() => {
@@ -177,7 +211,7 @@ export default function userDiary() {
                                         <label className="block text-sm font-medium text-gray-700 mb-1 dark:text-gray-300">Select Option</label>
                                         <select
                                             isRequired
-                                            className="block w-full p-4 border-gray-300 rounded-md shadow-sm dark:border-gray-600 dark:bg-[#2d3748] dark:text-gray-200 focus:outline-none focus:ring-2 focus:ring-[#6466f1] focus:border-transparent"
+                                            className="block w-full p-4 border-gray-300 rounded-md shadow-sm bg-slate-100 dark:border-gray-600 dark:bg-[#2d3748] dark:text-gray-200 focus:outline-none focus:ring-2 focus:ring-[#6466f1] focus:border-transparent"
                                             value={option}
                                             onChange={(e) => setOption(e.target.value)}
                                         >
@@ -214,7 +248,7 @@ export default function userDiary() {
                                                     variant="bordered"
                                                     value={reason}
                                                     onChange={(e) => setReason(e.target.value)}
-                                                    className="w-[43rem] dark:bg-[#2d3748] dark:text-gray-200"
+                                                    className="w-[43rem] bg-slate-100 dark:bg-[#2d3748] dark:text-gray-200"
                                                 />
                                             </div>
                                             <div className="mb-4 w-[43rem]">
@@ -227,14 +261,14 @@ export default function userDiary() {
                                                     variant="bordered"
                                                     value={cost}
                                                     onChange={(e) => setCost(e.target.value)}
-                                                    className="w-[43rem] dark:bg-[#2d3748] dark:text-gray-200"
+                                                    className="w-[43rem] bg-slate-100 dark:bg-[#2d3748] dark:text-gray-200"
                                                 />
                                             </div>
                                             <div className="mb-4">
                                                 <label className="block text-sm font-medium text-gray-700 mb-1 dark:text-gray-300">Hisab</label>
                                                 <select
                                                     isRequired
-                                                    className="block w-full p-4 border-gray-300 rounded-md shadow-sm dark:border-gray-600 dark:bg-[#2d3748] dark:text-gray-200 focus:outline-none focus:ring-2 focus:ring-[#6466f1] focus:border-transparent"
+                                                    className="block w-full p-4 border-gray-300 rounded-md shadow-sm dark:border-gray-600 bg-slate-100 dark:bg-[#2d3748] dark:text-gray-200 focus:outline-none focus:ring-2 focus:ring-[#6466f1] focus:border-transparent"
                                                     value={transactionType}
                                                     onChange={(e) => setTransactionType(e.target.value)}
                                                 // onChange={handelOparation}
@@ -247,13 +281,13 @@ export default function userDiary() {
                                         </>
                                     )}
                                     <Button type="submit" color="primary" className="mt-4 w-2/2" onClick={createProduct}>
-                                    {editingNoteId?"Update":"Create"}
+                                        {editingNoteId ? "Update" : "Create"}
                                     </Button>
                                 </form>
                             </ModalBody>
                             <ModalFooter className="flex justify-between">
 
-                                <Button color="danger" variant="light" onPress={closeReset} className="w-2/2 mr-2">
+                                <Button color="danger" variant="light" onPress={closeReset} className="w-1/1 mr-2">
                                     Close
                                 </Button>
                             </ModalFooter>
@@ -281,7 +315,7 @@ export default function userDiary() {
                             type="text"
                             value={searchQuery}
                             onChange={(e) => setSearchQuery(e.target.value)}
-                            placeholder="Search by title..."
+                            placeholder={`Search by ${searchItem !== "$.1" ? "Note" : "Spend at..."}`}
                             className="dark:text-gray-200 dark:bg-[#2d3748] p-2 rounded-md"
                         />
                         <select
@@ -290,7 +324,7 @@ export default function userDiary() {
                                 setPerPage(Number(e.target.value)); // Update perPage based on dropdown selection
                                 setCurrentPage(1); // Reset to first page when perPage changes
                             }}
-                            className="dark:!bg-[#2d3748] dark:!text-gray-200 !shadow-2xl !px-4 !py-3 !rounded-md !border !border-gray-300 dark:!border-gray-600 hover:!shadow-lg !transition-shadow cursor-pointer !duration-300"
+                            className="dark:!bg-[#2d3748] bg-slate-100 dark:!text-gray-200 !shadow-2xl !px-4 !py-3 !rounded-md !border !border-gray-300 dark:!border-gray-600 hover:!shadow-lg !transition-shadow cursor-pointer !duration-300"
                         >
                             <option className="cursor-pointer" value={5}>5</option>
                             <option className="cursor-pointer" value={10}>10</option>
@@ -298,6 +332,52 @@ export default function userDiary() {
                             <option className="cursor-pointer" value={20}>20</option>
                             <option className="cursor-pointer" value={50}>50</option>
                         </select>
+                        {/* <Select
+                            isRequired
+                            placeholder="Select search query"
+                            defaultSelectedKeys={["All"]}
+                            className="max-w-xs py-2"
+                            size="lg"
+                            color="dark:bg-[#2d3748]"
+                            value={searchItem}
+                            label='.'
+                            onChange={(value) => {
+                                setSearchItem(value.target.value);
+                            }}
+                        >
+                            <SelectItem value="all" className="dark:text-gray-200">All</SelectItem>
+                            <SelectItem value="note" className="dark:text-gray-200">Amount</SelectItem>
+                            <SelectItem value="amount" className="dark:text-gray-200">Note</SelectItem>
+
+                        </Select> */}
+                        <select
+                            isRequired
+                            className="block w-[15rem] p-4 border-gray-300 rounded-md shadow-sm dark:border-gray-600 bg-slate-100 dark:bg-[#2d3748] dark:text-gray-200 focus:outline-none focus:ring-2 focus:ring-[#6466f1] focus:border-transparent"
+                            value={searchItem}
+                            onChange={(e) => setSearchItem(e.target.value)}
+                        // onChange={handelOparation}
+                        >
+                            <option value="$.0">All</option>
+                            <option value="$.1">Amount</option>
+                            <option value="$.2">Note</option>
+                        </select>
+                        {
+                            searchItem === "$.1" ?
+                                <>
+                                    <select
+                                        isRequired
+                                        className="block w-[15rem] p-4 border-gray-300 rounded-md shadow-sm dark:border-gray-600 bg-slate-100 dark:bg-[#2d3748] dark:text-gray-200 focus:outline-none focus:ring-2 focus:ring-[#6466f1] focus:border-transparent"
+                                        value={transType}
+                                        onChange={(e) => setTransType(e.target.value)}
+                                    // onChange={handelOparation}
+                                    >
+                                        <option value="all">All</option>
+                                        <option value="debit">Debit</option>
+                                        <option value="credit">Credit</option>
+                                    </select>
+
+                                </> : ""
+                        }
 
                     </div>
 
@@ -338,9 +418,6 @@ export default function userDiary() {
                                                     <div className="flex gap-2">
 
                                                         <button onClick={() => editNote(blog._id)} className="dark:text-gray-100 dark:bg-[radial-gradient(black,transparent)] dark:hover:!bg-[#424f85] hover:!border-[#38457b]"><FaEdit /> Edit</button>
-                                                        {/* <Link href={`/privatenote/edit/${blog._id}`}>
-                                                            <button className="dark:text-gray-100 dark:bg-[radial-gradient(black,transparent)] dark:hover:!bg-[#424f85] hover:!border-[#38457b]"><FaEdit /> Edit</button>
-                                                        </Link> */}
                                                         <Link href={`/privatenote/delete/${blog._id}`}>
                                                             <button className="dark:text-gray-100 dark:bg-red-500"><RiDeleteBin6Fill /> Delete</button>
                                                         </Link>
