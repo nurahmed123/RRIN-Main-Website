6b7aca4fc41889cd21d247e46059de0814718770
Nur Ahmed <06nurahmed@gmail.com>
2024-12-15 17:53:20 +0600

    update

--- bitebd/data/index.ts
+++ bitebd/data/index.ts
@@ -18,7 +18,7 @@ export const gridItems = [
   },
   {
     id: 2,
-    title: "Leets Make Humger Free World.",
+    title: "Leets Make Hunger Free World.",
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-2",
     imgClassName: "",
