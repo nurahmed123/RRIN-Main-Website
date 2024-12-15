29af634e71338ccd341c58a577dce4b7041172e2
Nur Ahmed <06nurahmed@gmail.com>
2024-12-15 17:16:24 +0600

    Update Footer.tsx

--- bitebd/components/Footer.tsx
+++ bitebd/components/Footer.tsx
@@ -1,5 +1,4 @@
 import { FaLocationArrow } from "react-icons/fa6";
-
 import { socialMedia } from "@/data";
 import MagicButton from "./MagicButton";
 import Link from "next/link";
@@ -7,9 +6,9 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className="w-full pt-20 pb-10" id="contact">
-      {/* background grid */}
-      <div className="w-full absolute left-0 -bottom-72 min-h-96">
+    <footer className="w-full pt-20 pb-10 relative" id="contact">
+      {/* Background Grid */}
+      <div className="absolute inset-0 -z-10">
         <Image
           src="/footer-grid.svg"
           alt="grid"
@@ -20,7 +19,7 @@ const Footer = () => {
       </div>
 
       <div className="flex flex-col items-center">
-        <h1 className="heading lg:max-w-[45vw]">
+        <h1 className="heading lg:max-w-[45vw] text-center">
           <span className="text-purple">Join Us</span> in building a healthier world!
         </h1>
         <p className="text-white-200 md:mt-10 my-5 text-center">
@@ -34,27 +33,27 @@ const Footer = () => {
           />
         </a>
       </div>
-      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
-        <p className="md:text-base text-sm md:font-normal font-light">
-          Copyright © 2024 BiteBD
+
+      <div className="flex flex-col md:flex-row justify-between items-center mt-16">
+        <p className="md:text-base text-sm font-light">
+          Copyright &copy; 2024 BiteBD
         </p>
 
-        <div className="flex items-center md:gap-3 gap-6">
-          {socialMedia.map((info) => (
-            <div
-              key={info.id}
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
-            >
-              <Link target="_blank" href={info.url}>
+        <div className="flex items-center gap-6 md:gap-3">
+          {socialMedia.map(({ id, url, img }) => (
+            <Link key={id} href={url} target="_blank" passHref>
+              <div
+                className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+              >
                 <Image
-                  src={info.img}
-                  alt={`${info.img} icon`}
+                  src={img}
+                  alt={`${id} icon`}
                   width={20}
                   height={20}
                   className="rounded-lg"
                 />
-              </Link>
-            </div>
+              </div>
+            </Link>
           ))}
         </div>
       </div>
