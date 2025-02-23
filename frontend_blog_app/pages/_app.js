7cf92582b1f502c3b54efebad1ae45e6f13c2905
nurahmed123 <06nurahmed@gmail.com>
2025-02-23 21:12:22 +0600

    Update _app.js

--- frontend_blog_app/pages/_app.js
+++ frontend_blog_app/pages/_app.js
@@ -10,7 +10,7 @@ import { useState, useEffect } from 'react';
 import Script from "next/script"; // Import the Script component
 import "@/styles/tailwind.css"
 import "@/styles/globals.css";
-import Script from 'next/script';
+
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   const [loading, setLoading] = useState(true);
   const router = useRouter();
