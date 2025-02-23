7eb70b82a5d58403a4177e9b1fb5304d0edf92fb
Md Nur Ahmad <84973115+nurahmed123@users.noreply.github.com>
2025-02-24 00:30:52 +0600

    nadim

--- AI/app/layout.tsx
+++ AI/app/layout.tsx
@@ -1,7 +1,7 @@
 import RootLayout from './RootLayout'; // Import RootLayout from a separate file
 
 export const metadata = {
-  title: "RS AI",
+  title: "RoboSuperior AI",
   description: "Leets remove hunger together....",
 };
 
