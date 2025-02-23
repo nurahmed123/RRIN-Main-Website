1f27de4625590e89b9152b5bb43973a1e4fdf732
nurahmed123 <06nurahmed@gmail.com>
2025-02-23 17:39:58 +0600

    Update _document.js

--- frontend_blog_app/pages/_document.js
+++ frontend_blog_app/pages/_document.js
@@ -8,6 +8,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Outfit:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+        <meta name="2d9411ba8b77a336dc470fef38942249dbbecc18" content="2d9411ba8b77a336dc470fef38942249dbbecc18" />
       </Head>
       <body>
         <Main />
