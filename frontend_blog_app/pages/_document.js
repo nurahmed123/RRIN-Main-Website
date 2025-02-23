b6fdfa8307d2cd267f9ff845d7e52d31c2a57544
nurahmed123 <06nurahmed@gmail.com>
2025-02-23 20:35:11 +0600

    Update _document.js

--- frontend_blog_app/pages/_document.js
+++ frontend_blog_app/pages/_document.js
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-     <Head >
+      <Head >
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
