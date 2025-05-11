ca80e0e199b9e9577dd3d9bcf8d494a0aaa91162
raghib112 <06nurahmed@gmail.com>
2025-05-11 23:31:49 +0600

    adding file type

--- backend_admin_dashboard/next.config.mjs
+++ backend_admin_dashboard/next.config.mjs
@@ -11,6 +11,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'files.edgestore.dev',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn.hack.ngo',
+      },
     ],
   },
 };
