997212c0e515c5056bfa63af09149d5dbf78fde0
Md Nur Ahmad <84973115+nurahmed123@users.noreply.github.com>
2025-05-16 21:21:44 +0600

    ads stop

--- frontend_blog_app/components/ads/HilltopAdsScript.js
+++ frontend_blog_app/components/ads/HilltopAdsScript.js
@@ -1,40 +1,40 @@
-'use client';
+// 'use client';
 
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 
-const HilltopPOPAdsScript = () => {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = "//sentimental-glad.com/c/Dm9.6NbT2K5/lKSSW/QY9BN/jqEnxKO/TvYb2TN/i_0v2mMpTSEh5YN_jWY/3J";
-    script.async = true;
-    script.defer = true;
-    script.referrerPolicy = 'no-referrer-when-downgrade';
+// const HilltopPOPAdsScript = () => {
+//   useEffect(() => {
+//     const script = document.createElement('script');
+//     script.src = "//sentimental-glad.com/c/Dm9.6NbT2K5/lKSSW/QY9BN/jqEnxKO/TvYb2TN/i_0v2mMpTSEh5YN_jWY/3J";
+//     script.async = true;
+//     script.defer = true;
+//     script.referrerPolicy = 'no-referrer-when-downgrade';
 
-    document.body.appendChild(script);
+//     document.body.appendChild(script);
 
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
+//     return () => {
+//       document.body.removeChild(script);
+//     };
+//   }, []);
 
-  return null;
-};
+//   return null;
+// };
 
-const BannerScript = () => {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = "//unusedframe.com/bhX.VcsxdKGelf0PYsWCdainYjWt5Nu/ZnXNII/Rebm-9xu/ZXUqlXksPqTqYtxaMdj-YbwWNzjmErtpN-jbE/ywNQjMAQ2pMAgA";
-    script.async = true;
-    script.referrerPolicy = 'no-referrer-when-downgrade';
+// const BannerScript = () => {
+//   useEffect(() => {
+//     const script = document.createElement('script');
+//     script.src = "//unusedframe.com/bhX.VcsxdKGelf0PYsWCdainYjWt5Nu/ZnXNII/Rebm-9xu/ZXUqlXksPqTqYtxaMdj-YbwWNzjmErtpN-jbE/ywNQjMAQ2pMAgA";
+//     script.async = true;
+//     script.referrerPolicy = 'no-referrer-when-downgrade';
 
-    document.body.appendChild(script);
+//     document.body.appendChild(script);
 
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
+//     return () => {
+//       document.body.removeChild(script);
+//     };
+//   }, []);
 
-  return null;
-};
+//   return null;
+// };
 
 // export { HilltopPOPAdsScript, BannerScript };
